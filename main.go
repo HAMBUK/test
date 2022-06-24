f90664978ab1e852a3dcce957100ccd697fc5be3
HAMBUK <108109292+HAMBUK@users.noreply.github.com>
2022-06-24 12:45:27 +0900

    Add funcs

--- main.go
+++ main.go
@@ -4,6 +4,16 @@ import "log"
 
 func main() {
 	log.Println("hello world")
+	log.Println("sum 1+2 = ", sum(1, 2))
+	log.Println("sub 1-2 = ", sub(1, 2))
+}
+
+func sum(a int, b int) int {
+	return a + b
+}
+
+func sub(a int, b int) int {
+	return a * b
 }
 
 // tdsfs
